a3920bb508ab53bbdcc6e9c325f804c24eb6ad0f
Oleksandr Khoshaba <Oleksandr.Khoshaba@vntu.edu.ua>
2022-05-06 17:09:59 +0300

    initial status for 01

---
+++ 01/sparePartsInterface/src/main/java/org/vntu/sparePartsInterface/sparePartsInterface/SparePartsInterfaceApplication.java
@@ -0,0 +1,13 @@
+package org.vntu.sparePartsInterface.sparePartsInterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparePartsInterfaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparePartsInterfaceApplication.class, args);
+	}
+
+}
---
+++ 01/sparePartsInterface/src/test/java/org/vntu/sparePartsInterface/sparePartsInterface/SparePartsInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vntu.sparePartsInterface.sparePartsInterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparePartsInterfaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
